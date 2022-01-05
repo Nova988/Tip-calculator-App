44e8db4519245dbc65aa1227af479348bf281580
Nova988 <antonionovak1998@hotmail.com>
2022-01-05 20:08:30 +0100

    added calculator functionality

--- dist/main.js
+++ dist/main.js
@@ -1,24 +1,52 @@
 "use strict";
 
 // Variables
-const bill = document.getElementById("bill").value;
+let bill = document.getElementById("bill").value;
+let peopleAmount;
 const percentages = document.querySelectorAll('input[name="percentage"]');
-let selectedValue;
+let percentage;
 
-percentages.forEach((x) => {
-  if (x.checked) {
-    selectedValue = x.value;
-  }
-});
+let tipPerPerson;
+let totalPerPerson;
+let tipAmount = document.getElementById("tip-amount");
+let totalAmount = document.getElementById("total-amount");
+
+const customPercentage = document.getElementById("percentageCostum");
+console.log(customPercentage);
 
 window.addEventListener("keydown", function (e) {
   percentages.forEach((x) => {
     if (x.checked) {
-      selectedValue = x.value;
+      percentage = Number(x.value);
+      customPercentage.value = "";
     }
   });
+
   if (e.key === "Enter") {
-    console.log(bill);
-    console.log(selectedValue);
+    if (!percentage) {
+      percentage = Number(customPercentage.value) / 100;
+    }
+
+    bill = Number(document.getElementById("bill").value);
+    peopleAmount = Number(document.getElementById("amountOfPeople").value);
+
+    tipPerPerson = (bill * percentage) / peopleAmount;
+    totalPerPerson = bill / peopleAmount;
+
+    tipAmount.textContent = `$${tipPerPerson}`;
+    totalAmount.textContent = `$${totalPerPerson}`;
+
+    console.log(percentage);
+
+    percentage = 0;
+    console.log(percentage);
   }
 });
+
+customPercentage.onfocus = function () {
+  percentages.forEach((x) => {
+    if (x.checked) {
+      x.checked = false;
+    }
+  });
+};

97f0ee6d9d3ecd1431ff19083100520ebfcc0489
Nova988 <antonionovak1998@hotmail.com>
2022-01-08 16:36:02 +0100

    finished calculator functionality

--- dist/main.js
+++ dist/main.js
@@ -2,12 +2,12 @@
 
 // Variables
 let bill;
-const billEl = document.getElementById("bill");
 let peopleAmount;
 const percentages = Array.from(
   document.querySelectorAll('input[name="percentage"]')
 );
 const percentagesRadio = percentages.slice(0, -1);
+const resetButton = document.getElementById("button--reset");
 
 let percentage;
 
@@ -17,6 +17,14 @@ let tipAmount = document.getElementById("tip-amount");
 let totalAmount = document.getElementById("total-amount");
 
 const customPercentage = document.getElementById("percentageCostum");
+
+const billEl = document.getElementById("bill");
+const errorBill = document.getElementById("error--bill");
+
+const errorPercentage = document.querySelector(".error--percentage");
+
+const peopleEl = document.getElementById("amountOfPeople");
+const errorPeople = document.getElementById("error--people");
 
 percentagesRadio.forEach((x) => {
   x.addEventListener("focus", function () {
@@ -35,20 +43,63 @@ const calculation = function () {
 };
 
 const errorMessages = function () {
-  const billEl = document.getElementById("bill");
-  const errorBill = document.getElementById("error--bill");
-
-  const peopleEl = document.getElementById("amountOfPeople");
-  const errorPeople = document.getElementById("error--people");
   // error message bill
-  if (!bill) {
-    billEl.classList.add("error--bill");
+  if (!bill || bill <= 0) {
+    billEl.classList.add("error--border");
     errorBill.classList.remove("hide");
   }
-  if (!peopleAmount) {
-    peopleEl.classList.add("error--bill");
+
+  if (!peopleAmount || peopleAmount <= 0) {
+    peopleEl.classList.add("error--border");
     errorPeople.classList.remove("hide");
   }
+
+  if (!percentage || percentage <= 0) {
+    percentages[5].classList.add("error--border");
+    errorPercentage.classList.remove("hide");
+  }
+};
+
+const resetStyles = function () {
+  if (bill) {
+    billEl.classList.remove("error--border");
+    errorBill.classList.add("hide");
+  }
+
+  if (peopleAmount) {
+    peopleEl.classList.remove("error--border");
+    errorPeople.classList.add("hide");
+  }
+
+  if (percentage) {
+    percentages[5].classList.remove("error--border");
+    errorPercentage.classList.add("hide");
+  }
+};
+
+const resetCalculator = function () {
+  billEl.value = "";
+  customPercentage.value = "";
+
+  percentages.forEach((x) => {
+    if (x.checked) {
+      x.checked = false;
+    }
+  });
+
+  peopleEl.value = "";
+
+  tipAmount.textContent = "$0";
+  totalAmount.textContent = "$0";
+
+  billEl.classList.remove("error--border");
+  errorBill.classList.add("hide");
+
+  peopleEl.classList.remove("error--border");
+  errorPeople.classList.add("hide");
+
+  percentages[5].classList.remove("error--border");
+  errorPercentage.classList.add("hide");
 };
 
 window.addEventListener("keydown", function (e) {
@@ -67,10 +118,20 @@ window.addEventListener("keydown", function (e) {
     bill = Number(document.getElementById("bill").value);
     peopleAmount = Number(document.getElementById("amountOfPeople").value);
 
-    if (percentage && bill && peopleAmount) {
+    if (
+      percentage &&
+      percentage > 0 &&
+      bill &&
+      bill > 0 &&
+      peopleAmount &&
+      peopleAmount > 0
+    ) {
       calculation();
-    } else errorMessages();
-
+      resetStyles();
+    } else {
+      resetStyles();
+      errorMessages();
+    }
     percentage = 0;
   }
 });
@@ -82,3 +143,8 @@ customPercentage.onfocus = function () {
     }
   });
 };
+
+resetButton.addEventListener("click", function () {
+  resetStyles();
+  resetCalculator();
+});
